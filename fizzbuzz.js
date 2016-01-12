bc6cb1fd1cc1391108d96b371af200750b254ab9
jagoshj <jagoshj@mail.usf.edu>
2016-01-11 23:43:56 -0500

    fizz-buzz initial
    Initial js and html

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    for (var counter = 1; counter <= 100; counter++) {
+        if (counter % 15 == 0) {
+            console.log("fizzbuzz");
+            var btn = document.createElement("BUTTON");
+            var t = document.createTextNode("FIZZBUZZ");
+            btn.appendChild(t);
+            document.body.appendChild(btn);
+            document.write("<br>")
+        } else if (counter % 5 == 0) {
+            console.log("buzz");
+            var btn = document.createElement("BUTTON");
+            var t = document.createTextNode("BUZZ");
+            btn.appendChild(t);
+            document.body.appendChild(btn);
+            document.write("<br>")
+        } else if (counter % 3 == 0) {
+            console.log("fizz");
+            var btn = document.createElement("BUTTON");
+            var t = document.createTextNode("FIZZ");
+            btn.appendChild(t);
+            document.body.appendChild(btn);
+            document.write("<br>")
+        } else {
+            console.log(counter);
+            document.write(counter);
+            document.write("<br>")
+        }
+    }
+});
